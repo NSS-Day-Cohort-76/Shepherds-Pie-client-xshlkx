efc168345439228eb8e72fef4d9161ae51101028
Kyle Mims <kylemims@gmail.com>
2025-05-02 14:29:31 -0500

    merging with develop rebase

--- src/welcome/Welcome.jsx
+++ src/welcome/Welcome.jsx
@@ -4,9 +4,9 @@ export const Welcome = () => {
   return (
     <article className="welcome-container">
       <h1>
-        <span>Welcome to</span>
-        <span>Jakes Bakes</span>
+        <span>Welcome to Jakes Bakes</span>
       </h1>
+      <img className="welcome-image" src={`public/assets/jakeoff.png`} alt="Pizza" />
       <section>Buy a fuggin Pizza</section>
     </article>
   );
